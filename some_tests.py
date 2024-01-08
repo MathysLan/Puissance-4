ab9ce31c28a3bdc2e25a3eb0a21b0ac9c08fbc5b
Mathys Langiny <mathys.langiny@etudiant.univ-reims.fr>
2024-01-08 11:38:20 +0100

    Création de la fonction ajout du test de la fonction toString dans some_tests

--- some_tests.py
+++ some_tests.py
@@ -12,3 +12,14 @@ print(p)
 print("\x1B[43m \x1B[0m : carré jaune ")
 print("\x1B[41m \x1B[0m : carré rouge ")
 print("\x1B[41mA\x1B[0m : A sur fond rouge")
+
+
+from Model.Constantes import *
+from Model.Plateau import *
+from Model.Pion import *
+from random import randint, choice
+p = construirePlateau()
+for _ in range(20):
+ placerPionPlateau(p, construirePion(choice(const.COULEURS)),
+ randint(0, const.NB_COLUMNS - 1))
+print(toStringPlateau(p))
